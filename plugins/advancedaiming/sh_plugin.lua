27fa5e3facaba6a65ff1ebca0dfda2d25f0ac44f
VindoesCompooter <100soundsystem32@gmail.com>
2021-04-08 00:01:13 +0100

    Advanced Aiming
    Adds advanced aiming and camera for players to use.

---
+++ plugins/advancedaiming/sh_plugin.lua
@@ -0,0 +1,7 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Advanced Aiming"
+PLUGIN.author = "Seamus"
+PLUGIN.desc = "Smoother aiming to assist in more tactical play."
+
+nut.util.include("cl_plugin.lua")
